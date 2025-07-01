c859d0e340296e2a92a57e69732e125a7177ff49
biyrololo <porkalox@gmail.com>
2025-07-01 13:09:47 +0300

    fix: getMe null if no email

--- src/shared/lib/actions/auth/getMe.ts
+++ src/shared/lib/actions/auth/getMe.ts
@@ -3,6 +3,7 @@ import { prisma } from "@/shared/lib/prisma";
 
 export async function getMe() {
     const session = await getServerSession();
+    if(!session?.user?.email) return null;
     const user = await prisma.user.findUnique({
         where: {
             email: session?.user?.email
